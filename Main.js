d6b7c0c5d9028487782e4c5191faa1158001791a
E-Khangal <146406961+E-Khangal@users.noreply.github.com>
2023-10-27 03:43:48 +0000

    х

--- Main.js
+++ Main.js
@@ -1,8 +1,25 @@
-let age=prompt("Тоогоо оруулна уу ")
-if (age >=18){
-alert=("cu-ээс гоймон авч болохгүй")}
-else {alert=("cu-ээс гоймон авч болно")}
+/*let age=prompt("Тоогоо оруулна уу ")
+if(age>=18){alert=("cu-ээс гоймон авч болохгүй")}
+else {alert=("cu-ээс гоймон авч болно")}*/
+
+
+
+let newpasswort=prompt("Nuuts ugee oruulna uu");
+let comfirmNewpassword;
+if(newpasswort==null)
+{alert=("nuuts ug hooson bn")
+newpasswort=prompt ("Шинэ нууц үгээ оруулна уу");}
+else {comfirmNewpassword=prompt ("Шинэ нууц үгийг давтан оруулна уу")};
+if(newpasswort===comfirmNewpassword)
+{alert=("Нууц үг амжилттай солигдлоо.")}
+else {alert=("Нууц үгээ зөв давтаж оруулаарай")}
+{comfirmNewpassword=prompt ("Зөв нууц үгээ давтан оруулна уу")};
+comfirmNewpassword=prompt("Шинэ нууц үгийг давтан оруулна уу");
+if(comfirmNewpassword===comfirmNewpassword)
+{alert=("Нууц үг амжилттай солигдлоо.")}
 
 
 
 
+/*let шөл=prompt("Шөлтэй устай хоол идэх үү? Тийм бол 1, Үгүй бол 2 гэж оруул");
+ let tsai, tatsanmah, bansh, yutaishol; */
