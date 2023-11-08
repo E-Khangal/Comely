18f9286b3f7cab904c8a6bd81038cc42c1a46f89
E-Khangal <146406961+E-Khangal@users.noreply.github.com>
2023-11-08 13:10:45 +0000

    P

---
+++ a.js
@@ -0,0 +1,36 @@
+/*const calculate=(B,C,D)=>{
+    return A=B*C-D;
+}
+console.log("A=",calculate(5,10,3));*/
+
+
+
+/*const totalTime=(day,time)=>{
+    return Alltime=day*24+time;
+}
+console.log("Alltime=",totalTime(3,10));*/
+
+
+
+/*const max=(A,B)=>{
+    return ihtoo=;
+}
+console.log("ihtoo:",max(10,20));*/
+
+
+
+
+/*const calculateArea=(orgon,urt)=>{
+    return TegshOntsogtiinTalbaiNi=urt*orgon;
+}
+console.log("TegshOntsogtiinTalbaiNi=",calculateArea(20,30));*/
+
+
+const checkPoint=(k)=>{
+    return UsgenUnelgee=
+     90<=A<=100,
+     80<B<90,
+     70<=C<80,
+     60<=D<70,
+     0<=F<60;}
+console.log("UsgenUnelgee:",checkPoint(95));
